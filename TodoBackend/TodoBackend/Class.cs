eef24cb71ca3cad3bbd672084e91f59465385a30
illay123 <illay.manor@gmail.com>
2020-10-01 16:41:01 +0300

    uploaded leaning task

---
+++ TodoBackend/TodoBackend/Class.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoBackend
+{
+    public class TodoBuildData
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string Content { get; set; }
+
+        public TodoBuildData()
+        {
+        }
+
+        public TodoBuildData(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+
+    public class TodoModel
+    {
+        public string Id;
+        public string Title;
+        public string Content;
+        public bool IsDone;
+
+        public TodoModel(string title, string content)
+        {
+            Id = Guid.NewGuid().ToString();
+            Title = title;
+            Content = content;
+            IsDone = false;
+        }
+    }
+
+    public interface IDbConnector
+    {
+        public IEnumerable<TodoModel> Query(string query);
+        public IEnumerable<TodoModel> All();
+        public bool Insert(string Title, string Content);
+        public bool Delete(string id);
+        public int DeleteAllDone();
+        public bool finish(string id);
+
+    }
+
+    public class MemDb : IDbConnector
+    {
+        private List<TodoModel> db;
+
+        public MemDb()
+        {
+            db = new List<TodoModel>
+            {
+                new TodoModel("learn asp.net", "3 days project"),
+                new TodoModel("get shit done", "right now")
+            };
+        }
+
+        public IEnumerable<TodoModel> Query(string query)
+        {
+            return db.Where((x) => x.Id.ToString() == query);
+        }
+
+        public bool Insert(string Title, string Content)
+        {
+            var model = new TodoModel(Title, Content);
+            if (db.Where((x) => x.Id == model.Id).Any()){
+                return false;
+            }
+            else { 
+                db.Add(model);
+                return true;
+            }
+        }
+
+        public IEnumerable<TodoModel> All()
+        {
+            return db;
+        }
+
+        public bool Delete(string id)
+        {                
+            return db.RemoveAll((x) => x.Id == id) > 0;
+        }
+
+        public int DeleteAllDone()
+        {
+            return db.RemoveAll((x) => x.IsDone = true);
+        }
+
+        public bool finish(string id)
+        {
+            if (!db.Where((x) => x.Id == id).Any())
+            {
+                return false;
+            }
+            else
+            {
+                db.ForEach(x => { x.IsDone = x.IsDone || x.Id == id;});
+                return true;
+            }
+        }
+    }
+}
---
+++ TodoBackend/TodoBackend/Controllers/TodoController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft;
+
+namespace TodoBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+        private readonly IDbConnector _connector;
+
+        public TodoController(ILogger<TodoController> logger, IDbConnector connector)
+        {
+            _logger = logger;
+            _connector = connector;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(_connector.All().Select(x=> Newtonsoft.Json.JsonConvert.SerializeObject(x)).ToArray());
+        }
+
+        [HttpPost]
+        public bool Post([FromBody] TodoBuildData data)
+        {
+            return _connector.Insert(data.Title, data.Content);
+        }
+
+        [Route("finish/{id:Guid}")]
+        public bool Finish(string id)
+        {
+            return _connector.finish(id);
+        }
+
+        [Route("delete/{id:Guid}")]
+        public bool Delete(string id)
+        {
+            return _connector.Delete(id);
+        }
+
+        [Route("delete/all_finished")]
+        public int DeleteAll()
+        {
+            return _connector.DeleteAllDone();
+        }
+    }
+}
